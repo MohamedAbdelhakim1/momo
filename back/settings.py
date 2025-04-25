4c125bb2ecdf6e6f1a5af66de6f97a7c1d3e5bb3
MohamedAbdelhakim1 <momofaheam@gmail.com>
2025-04-26 01:52:42 +0300

    Update settings: set DEBUG to False and adjust STATIC_URL path

--- back/settings.py
+++ back/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-#l&-55g6!v+qf5zysyqbr$i_86+z1ux73(qfx4z*#%j_lcq^cs"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_URL = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
